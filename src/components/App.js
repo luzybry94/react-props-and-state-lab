24dab19f9fb09cc1a4674c639950dfecc36bfd25
luzybry94 <luzybry94@gmail.com>
2021-04-27 19:26:17 -0700

    commit tree 4ed6c161c5f3105b18883c64d3554124896b3a4a

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,38 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (event) => {
+    this.setState({
+      filters: {
+        type: event.target.value
+      }
+    })
+  }
+
+  onFindPetsClick = () => {
+    let url = '/api/pets'
+    if (this.state.filters.type !== 'all') {
+      url += `?type=${this.state.filters.type}`
+    }
+
+    fetch(url)
+      .then(res => res.json())
+      .then(petsArr => {
+        this.setState({
+          pets: petsArr
+        })
+      })
+  }
+
+  onAdoptPet = (id) => {
+    const pets = [...this.state.pets]
+    const pet = pets.find(pet => pet.id === id)
+    pet.isAdopted = true
+    this.setState({
+      pets: pets
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +56,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
